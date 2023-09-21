fda861c560d861ab99de47d7c49c2293834deb66
belyaeval <belen@yandex.ru>
2023-09-21 18:57:30 +0700

    Mongo db hw fix update

--- hw-08-mongo-db/src/main/java/ru/otus/springhw/service/AuthorServiceImpl.java
+++ hw-08-mongo-db/src/main/java/ru/otus/springhw/service/AuthorServiceImpl.java
@@ -3,8 +3,10 @@ package ru.otus.springhw.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import ru.otus.springhw.domain.Book;
 import ru.otus.springhw.repository.AuthorRepository;
 import ru.otus.springhw.domain.Author;
+import ru.otus.springhw.repository.BookRepository;
 
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -14,6 +16,8 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class AuthorServiceImpl implements AuthorService {
     private final AuthorRepository authorRepository;
+
+    private final BookRepository bookRepository;
 
     @Override
     public Optional<Author> findById(String id) {
@@ -44,6 +48,14 @@ public class AuthorServiceImpl implements AuthorService {
     public Author updateById(String id, String name) {
         Author authorToUpdate = findById(id).orElseThrow(NoSuchElementException::new);
         authorToUpdate.setName(name);
+
+        List<Book> books = bookRepository.findAll();
+
+        books.stream().filter((book) -> book.getAuthor().getId().equals(id))
+                .forEach((book) -> {
+                    book.setAuthor(authorToUpdate);
+                    bookRepository.save(book);
+                });
 
         return authorRepository.save(authorToUpdate);
     }
